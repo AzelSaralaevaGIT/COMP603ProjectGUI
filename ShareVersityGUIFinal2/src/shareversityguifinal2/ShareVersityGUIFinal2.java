d66967fd0a30711d83b7c96feb748a4057cf484a
azelsaralaeva <saralaeva.azel@gmail.com>
2023-10-05 16:02:05 +1300

    main

---
+++ ShareVersityGUIFinal2/src/shareversityguifinal2/ShareVersityGUIFinal2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shareversityguifinal2;
+
+/**
+ *
+ * @author saral
+ */
+public class ShareVersityGUIFinal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("I am testing youuuu");
+    }
+    
+}
