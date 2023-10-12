b1816a89694632ebcb772ff571ba020b2d808234
azelsaralaeva <saralaeva.azel@gmail.com>
2023-10-12 14:33:51 +1300

    database amendments

--- ShareVersityGUIFinal2/src/shareversityguifinal2/ShareVersityDatabase.java
+++ ShareVersityGUIFinal2/src/shareversityguifinal2/ShareVersityDatabase.java
@@ -23,22 +23,22 @@ public class ShareVersityDatabase
         conn = dbManager.getConnection();
     }
     
-     public void connectBookStoreDB() {
+     public void connectShareVersityDB() {
         //use the conn, initialize database by creating BOOK Table and insert records
         try
         {
             this.statement = conn.createStatement();
-            this.checkExistedTable("BOOK");
+            this.checkExistedTable("ACCOUNTINFO");
             
             this.statement.addBatch("CREATE TABLE ACCOUNTINFO (USERNAME VARCHAR(50), PASSWORD VARCHAR(50), FULLNAME VARCHAR(50), BANKACCOUNTNUMBER INT, DATEOFBIRTH VARCHAR(40), WALLETAMOUNT FLOAT)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('AZEL00', 'Slum Dog Millionaire', 'Fiction', 19.90)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('FRAN123', 'Run Mummy Run', 'Fiction', 28.00)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('MARY1', 'The Land of Painted Caves', 'Fiction', 15.40)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('JORDAN99', 'Cook with Jamie', 'Non-fiction', 55.20)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('SARAHMA33', 'Far Eastern Odyssey', 'Non-fiction', 24.90)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES (ANAHIT35, 'Open', 'Non-fiction', 33.60)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES (LUKE77, 'Big Java', 'Textbook', 55.00)");
-            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES (JOHNSMITH51, 'Financial Accounting', 'Textbook', 24.80)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('AZEL00', 'password123', 'Azel Saralaeva', 714342652622, '20 May 2000', 10000.00)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('FRAN123', 'academic111', 'Fran Caveman', 658425618632, '10 August 1998', 56000.00)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('MARY1', 'comp603', 'Mary Johnstonson', 524897521456, '5 December 1967', 33000.00)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('JORDAN99', 'tvtv33', 'Jordan Smith', 432895547812, '1 July 2005', 500.00)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES ('SARAHMA33', 'giveus100percentplease', 'Sarah Maverye', 714342652622, '20 June 1994', 10000.00)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES (ANAHIT35, 'weworkedhardonthis', 'Ana Hit', 985263418523, '17 January 1977', 5555.00)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES (LUKE77, 'thankyou!', 'Luke Monte', 55678952136202, '8 September 2001', 78564.00)");
+            this.statement.addBatch("INSERT INTO ACCOUNTINFO VALUES (JOHNSMITH51, 'hehehe', 'John Smith', 714342652622, '12 August 1989', 3345.00)");
             
             this.statement.executeBatch();
         }
@@ -79,7 +79,7 @@ public class ShareVersityDatabase
         }
     }
     
-    public ResultSet getWeekSpecial() {
+    public ResultSet getAccountInfo() {
         /* You may need the following SQL statements for this method:
 
         * Query multiple tables:
@@ -103,29 +103,7 @@ public class ShareVersityDatabase
         return (rs);
     }
 
-    public void createWeekSpecialTable(ResultSet rs) 
-    {
-        try 
-        {
-            this.checkExistedTable("WEEKSPECIAL");
-            this.statement = conn.createStatement();
-            this.statement.addBatch("CREATE TABLE WEEKSPECIAL (TITLE VARCHAR(50), SPECIALPRICE FLOAT)");
-            while (rs.next()) {
-                String title = rs.getString("TITLE");
-                float price = rs.getFloat("PRICE");
-                int discount = rs.getInt("DISCOUNT");
-                float newPrice = price * (100 - discount) / 100;
-                this.statement.addBatch("INSERT INTO WEEKSPECIAL VALUES('" + title + "', " + newPrice + ")");
-            }
-            this.statement.executeBatch();
-            rs.close();
-        }
-        
-        catch (SQLException ex) 
-        {
-            System.out.println(ex.getMessage());
-        }
-    }
+  
     
     public void checkExistedTable(String name)
     {
---
+++ ShareVersityGUIFinal2/src/shareversityguifinal2/ShareVersityDatabaseMain.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shareversityguifinal2;
+
+/**
+ *
+ * @author saral
+ */
+public class ShareVersityDatabaseMain {
+ 
+    public static void main(String[] args) {
+        
+        ShareVersityDatabase database = new ShareVersityDatabase();
+        database.connectShareVersityDB();
+        
+    }
+
+    
+    }
+    
+  
