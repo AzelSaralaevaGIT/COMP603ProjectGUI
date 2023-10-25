8f99099d84cde70d518b6ffb30d8cef80940f59f
azelsaralaeva <saralaeva.azel@gmail.com>
2023-10-25 21:25:05 +1300

    databasemain update

--- ShareVersityGUIFinal2/src/shareversityguifinal2/ShareVersityDatabaseMain.java
+++ ShareVersityGUIFinal2/src/shareversityguifinal2/ShareVersityDatabaseMain.java
@@ -1,18 +1,46 @@
 
 package shareversityguifinal2;
 
+import org.apache.derby.iapi.sql.ResultSet;
+
 /**
  *
  * @author saral
  */
 public class ShareVersityDatabaseMain {
  
+
     public static void main(String[] args) {
-        
-        ShareVersityDatabase database = new ShareVersityDatabase();
-        database.connectShareVersityDB();
-        
+        ShareVersityDatabase shareVersityDB = new ShareVersityDatabase();
+
+        // Connect to the database and create the table and insert data
+        shareVersityDB.connectShareVersityDB();
+
+        // Perform database operations, for example, retrieve account information
+        ResultSet accountInfo = (ResultSet) shareVersityDB.getAccountInfo();
+        try {
+            while (accountInfo.next()) {
+                String username = accountInfo.getString("USERNAME");
+                String fullName = accountInfo.getString("FULLNAME");
+                int bankAccountNumber = accountInfo.getInt("BANKACCOUNTNUMBER");
+                String dateOfBirth = accountInfo.getString("DATEOFBIRTH");
+                double walletAmount = accountInfo.getDouble("WALLETAMOUNT");
+
+                System.out.println("Username: " + username);
+                System.out.println("Full Name: " + fullName);
+                System.out.println("Bank Account Number: " + bankAccountNumber);
+                System.out.println("Date of Birth: " + dateOfBirth);
+                System.out.println("Wallet Amount: " + walletAmount);
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Close the database connection
+        shareVersityDB.closeConnection();
     }
+
 
     
     }
